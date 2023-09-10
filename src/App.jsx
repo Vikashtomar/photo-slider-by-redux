4d962861f690997d6d28ec8c41f8cd5a787232b9
vikash <vikash.tomar.india@gmail.com>
2023-09-10 22:08:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
  import React, { useEffect } from 'react'
  import {useDispatch,useSelector} from 'react-redux'
+
  import './App.css'
  import axios from 'axios'
- import { setProductData,setCurrentIndex, setnextProduct, setPrevious } from './Slider'
+ import { setProductData,setnextProduct, setPrevious } from './Slider'
+
+
  function App() {
+
   const dispatch = useDispatch();
   const products = useSelector((state)=>  state.product.products)
   const currentIndex = useSelector((state)=>state.product.currentIndex);
@@ -12,11 +16,11 @@
     axios.get("https://fakestoreapi.com/products")
     .then((res)=>{
       console.log(res.data)
-      dispatch(setProductData(res.data));
-      
+      dispatch(setProductData(res.data));      
     })
-
   },[dispatch])
+
+  console.log(products)
 
   function handlenext()
   {
@@ -27,14 +31,18 @@
   {
     dispatch(setPrevious())
   }
+
    return (
      <div>
       <div className="box">
-        <img src={products[currentIndex].image} alt="mypic" />
-        <h3>{products[currentIndex].title}</h3>
+              
+        <img src={products[currentIndex].image} alt="mypic" /> 
 
+        <h3>{products[currentIndex].title}</h3>     
       </div>
+      
       <button onClick={handlePrevious} >Previous</button>
+      <br />
       <button  onClick={handlenext}>Next</button>
      </div>
    )
